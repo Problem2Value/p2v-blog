eb120c8328a3a1321a31156311b80f7365a8957a
Geert <geert@problem2value.com>
2021-03-18 16:33:16 +0100

    Improve template

---
+++ components/BlogHero.vue
@@ -0,0 +1,44 @@
+<template>
+  <section id="hero" class="text-gray-600 body-font">
+    <div
+      class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center"
+    >
+      <div
+        class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center"
+      >
+        <h1 class="font-semibold text-7xl sm:text-4xl mb-4 text-gray-900">
+          Distributed Innovation Work
+        </h1>
+        <p class="mb-8 leading-relaxed text-2xl">
+          A blog about anything related to innovation methodologies, frameworks
+          & technique as well as distributed work (with a focus on IT in Europe)
+        </p>
+        <!-- <div class="flex justify-center">
+          <button
+            class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+          >
+            Problem2Value
+          </button>
+          <button
+            class="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg"
+          >
+            IT Hires Europe
+          </button>
+        </div> -->
+      </div>
+      <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+        <img
+          class="object-cover object-center rounded"
+          alt="hero"
+          src="../static/img/DistributedInnovationWork.svg"
+        />
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,65 +1,361 @@
 <template>
-  <div>
-    <div class="sticky w-full border-b bg-white border-gray-300">
-      <div class="container mx-auto lg px-4 sm:px-6">
-        <div
-          class="flex justify-between items-center py-4 md:justify-start md:space-x-10"
-        >
-          <div class="flex justify-start lg:w-0 lg:flex-1">
-            <a href="#">
-              <span class="sr-only">Workflow</span>
+  <!-- Later add styles for dark mode -->
+  <!-- <div class="text-gray-700 bg-white dark:bg-gray-800"> -->
+  <div class="text-gray-700 bg-white">
+    <AppHeader />
+
+    <BlogHero />
+
+    <section id="blog-articles" class="text-gray-600 body-font">
+      <div class="container px-5 py-24 mx-auto">
+        <div class="flex flex-wrap -m-4">
+          <div class="p-4 md:w-1/3">
+            <div
+              class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden"
+            >
               <img
-                src="https://uploads-ssl.webflow.com/603505e39067d3821a008fff/603506bbea94ae409c67673f_ITHiresLogo.svg"
-                width="233"
-                alt=""
+                class="lg:h-48 md:h-36 w-full object-cover object-center"
+                src="../static/img/missingthemark.png"
+                alt="blog"
               />
-            </a>
+              <div class="p-6">
+                <h2
+                  class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1"
+                >
+                  CATEGORY
+                </h2>
+                <h1 class="title-font text-lg font-medium text-gray-900 mb-3">
+                  The Catalyzer
+                </h1>
+                <p class="leading-relaxed mb-3">
+                  Photo booth fam kinfolk cold-pressed sriracha leggings
+                  jianbing microdosing tousled waistcoat.
+                </p>
+                <div class="flex items-center flex-wrap">
+                  <a
+                    class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0"
+                    >Learn More
+                    <svg
+                      class="w-4 h-4 ml-2"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      fill="none"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    >
+                      <path d="M5 12h14"></path>
+                      <path d="M12 5l7 7-7 7"></path>
+                    </svg>
+                  </a>
+                </div>
+              </div>
+            </div>
           </div>
 
-          <div class="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
-            <a
-              href="#"
-              class="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-red-600 hover:bg-yellow-400"
+          <div
+            v-for="article of articles"
+            :key="article.slug"
+            class="p-4 md:w-1/3"
+          >
+            <div
+              class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden"
             >
-              Hire Top Talent
-            </a>
+              <img
+                class="lg:h-48 md:h-36 w-full object-cover object-center"
+                :src="article.img"
+                alt="blog"
+              />
+              <div class="p-6">
+                <h2
+                  class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1"
+                >
+                  CATEGORY
+                </h2>
+                <h1 class="title-font text-lg font-medium text-gray-900 mb-3">
+                  {{ article.title }}
+                </h1>
+                <p class="leading-relaxed mb-3">
+                  {{ article.description }}
+                </p>
+                <div class="flex items-center flex-wrap">
+                  <a
+                    class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0"
+                    >Learn More
+                    <svg
+                      class="w-4 h-4 ml-2"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      fill="none"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    >
+                      <path d="M5 12h14"></path>
+                      <path d="M12 5l7 7-7 7"></path>
+                    </svg>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <!-- <div class="p-4 md:w-1/3">
+            <div
+              class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden"
+            >
+              <img
+                class="lg:h-48 md:h-36 w-full object-cover object-center"
+                src="https://dummyimage.com/721x401"
+                alt="blog"
+              />
+              <div class="p-6">
+                <h2
+                  class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1"
+                >
+                  CATEGORY
+                </h2>
+                <h1 class="title-font text-lg font-medium text-gray-900 mb-3">
+                  The 400 Blows
+                </h1>
+                <p class="leading-relaxed mb-3">
+                  Photo booth fam kinfolk cold-pressed sriracha leggings
+                  jianbing microdosing tousled waistcoat.
+                </p>
+                <div class="flex items-center flex-wrap">
+                  <a
+                    class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0"
+                    >Learn More
+                    <svg
+                      class="w-4 h-4 ml-2"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      fill="none"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    >
+                      <path d="M5 12h14"></path>
+                      <path d="M12 5l7 7-7 7"></path>
+                    </svg>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="p-4 md:w-1/3">
+            <div
+              class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden"
+            >
+              <img
+                class="lg:h-48 md:h-36 w-full object-cover object-center"
+                src="https://dummyimage.com/722x402"
+                alt="blog"
+              />
+              <div class="p-6">
+                <h2
+                  class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1"
+                >
+                  CATEGORY
+                </h2>
+                <h1 class="title-font text-lg font-medium text-gray-900 mb-3">
+                  Shooting Stars
+                </h1>
+                <p class="leading-relaxed mb-3">
+                  Photo booth fam kinfolk cold-pressed sriracha leggings
+                  jianbing microdosing tousled waistcoat.
+                </p>
+                <div class="flex items-center flex-wrap">
+                  <a
+                    class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0"
+                    >Learn More
+                    <svg
+                      class="w-4 h-4 ml-2"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      fill="none"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    >
+                      <path d="M5 12h14"></path>
+                      <path d="M12 5l7 7-7 7"></path>
+                    </svg>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div> -->
+        </div>
+      </div>
+    </section>
+
+    <AppFooter />
+
+    <!-- <footer class="bg-gray-100 text-gray-600 body-font">
+      <div
+        id="footer-links"
+        class="container px-5 py-24 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col"
+      >
+        <div
+          class="w-64 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left md:mt-0 mt-10"
+        >
+          <a
+            class="flex title-font font-medium items-center md:justify-start justify-center text-gray-900"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
+              viewBox="0 0 24 24"
+            >
+              <path
+                d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"
+              ></path>
+            </svg>
+            <span class="ml-3 text-xl">Tailblocks</span>
+          </a>
+          <p class="mt-2 text-sm text-gray-500">
+            Air plant banjo lyft occupy retro adaptogen indego
+          </p>
+        </div>
+        <div
+          class="flex-grow flex flex-wrap md:pr-20 -mb-10 md:text-left text-center order-first"
+        >
+          <div class="lg:w-1/4 md:w-1/2 w-full px-4">
+            <h2
+              class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3"
+            >
+              CATEGORIES
+            </h2>
+            <nav class="list-none mb-10">
+              <li>
+                <a class="text-gray-600 hover:text-gray-800">First Link</a>
+              </li>
+              <li>
+                <a class="text-gray-600 hover:text-gray-800">Second Link</a>
+              </li>
+              <li>
+                <a class="text-gray-600 hover:text-gray-800">Third Link</a>
+              </li>
+              <li>
+                <a class="text-gray-600 hover:text-gray-800">Fourth Link</a>
+              </li>
+            </nav>
+          </div>
+          <div class="lg:w-1/4 md:w-1/2 w-full px-4">
+            <h2
+              class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3"
+            >
+              CATEGORIES
+            </h2>
+            <nav class="list-none mb-10">
+              <li>
+                <a class="text-gray-600 hover:text-gray-800">First Link</a>
+              </li>
+              <li>
+                <a class="text-gray-600 hover:text-gray-800">Second Link</a>
+              </li>
+              <li>
+                <a class="text-gray-600 hover:text-gray-800">Third Link</a>
+              </li>
+              <li>
+                <a class="text-gray-600 hover:text-gray-800">Fourth Link</a>
+              </li>
+            </nav>
           </div>
         </div>
       </div>
-    </div>
-
-    <div class="container mx-auto lg px-4 sm:px-6 py-5">
-      <h1 class="font-bold text-4xl pb-5">Blog Posts</h1>
-
-      <ul class="flex flex-wrap">
-        <li
-          v-for="article of articles"
-          :key="article.slug"
-          class="xs:w-full md:w-1/2 px-2 xs:mb-6 md:mb-12 article-card"
+      <div id="footer-copyright" class="bg-black">
+        <div
+          class="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row"
         >
-          <NuxtLink
-            :to="{ name: 'blog-slug', params: { slug: article.slug } }"
-            class="flex transition-shadow duration-150 ease-in-out shadow-sm hover:shadow-md xxlmax:flex-col"
+          <p class="text-white text-sm text-center sm:text-left">
+            © 2021
+            <a
+              href="https://problem2value.com"
+              rel="noopener noreferrer"
+              class="text-white ml-1"
+              target="_blank"
+              >Problem2Value</a
+            >
+          </p>
+          <span
+            class="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start"
           >
-            <img :src="article.img" />
-            <div>
-              <h2 class="font-bold">{{ article.title }}</h2>
-              <p class="font-bold text-gray-600 text-sm">
-                {{ article.description }}
-              </p>
-            </div>
-          </NuxtLink>
-        </li>
-      </ul>
-    </div>
-    <footer class="flex justify-center bg-black">
+            <a class="ml-3 text-white">
+              <svg
+                fill="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                class="w-5 h-5"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"
+                ></path>
+              </svg>
+            </a>
+
+            <a class="ml-3 text-white">
+              <svg
+                fill="currentColor"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="0"
+                class="w-5 h-5"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  stroke="none"
+                  d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"
+                ></path>
+                <circle cx="4" cy="4" r="2" stroke="none"></circle>
+              </svg>
+            </a>
+          </span>
+        </div>
+      </div>
+    </footer> -->
+
+    <!-- <h1 class="font-bold text-4xl pb-5">Articles</h1>
+
+        <ul class="flex flex-wrap">
+          <li
+            v-for="article of articles"
+            :key="article.slug"
+            class="xs:w-full md:w-1/2 px-2 xs:mb-6 md:mb-12 article-card"
+          >
+            <NuxtLink
+              :to="{ name: 'blog-slug', params: { slug: article.slug } }"
+              class="flex transition-shadow duration-150 ease-in-out shadow-sm hover:shadow-md xxlmax:flex-col"
+            >
+              <img :src="article.img" />
+              <div>
+                <h2 class="font-bold">{{ article.title }}</h2>
+                <p class="font-bold text-gray-600 text-sm">
+                  {{ article.description }}
+                </p>
+              </div>
+            </NuxtLink>
+          </li>
+        </ul>
+      </div>
+    </section> -->
+    <!-- <footer class="flex justify-center bg-black">
       <p class="mt-4 text-white">
         Articles by
         <a href="https://it-hires.eu" class="font-bold hover:underline"
           >IT Hires Europe</a
         >
       </p>
-    </footer>
+    </footer> -->
   </div>
 </template>
 
