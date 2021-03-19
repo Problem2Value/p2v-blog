8b4b9f997a96f9053fcbc7e5c256bc3ad68d18a6
Geert <geert@problem2value.com>
2021-03-19 11:34:41 +0100

    add quotes again

--- pages/index.vue
+++ pages/index.vue
@@ -11,136 +11,54 @@
           <div
             v-for="article of articles"
             :key="article.slug"
-            class="p-4 md:w-1/3"
+            class="flex-1 p-4 md:w-1/3"
           >
-            <div
-              class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden"
+            <NuxtLink
+              :to="{ name: 'blog-slug', params: { slug: article.slug } }"
+              class="flex transition-shadow duration-150 ease-in-out shadow-sm hover:shadow-md xxlmax:flex-col"
             >
-              <img
-                class="lg:h-48 md:h-36 w-full object-cover object-center"
-                :src="article.thumbnail"
-                alt="blog"
-              />
-              <div class="p-6">
-                <h2
-                  class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1"
-                >
-                  CATEGORY
-                </h2>
-                <h1 class="title-font text-lg font-medium text-gray-900 mb-3">
-                  {{ article.title }}
-                </h1>
-                <p class="leading-relaxed mb-3">
-                  {{ article.description }}
-                </p>
-                <div class="flex items-center flex-wrap">
-                  <a
-                    class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0"
-                    >Learn More
-                    <svg
-                      class="w-4 h-4 ml-2"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      fill="none"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    >
-                      <path d="M5 12h14"></path>
-                      <path d="M12 5l7 7-7 7"></path>
-                    </svg>
-                  </a>
+              <div
+                class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden"
+              >
+                <img
+                  class="lg:h-48 md:h-36 w-full object-cover object-center"
+                  :src="article.thumbnail"
+                  alt="blog"
+                />
+                <div class="p-6">
+                  <h2
+                    class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1"
+                  >
+                    CATEGORY
+                  </h2>
+                  <h1 class="title-font text-lg font-medium text-gray-900 mb-3">
+                    {{ article.title }}
+                  </h1>
+                  <p class="leading-relaxed mb-3">
+                    {{ article.description }}
+                  </p>
+                  <div class="flex items-center flex-wrap">
+                    <a
+                      class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0"
+                      >Learn More
+                      <svg
+                        class="w-4 h-4 ml-2"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        stroke-width="2"
+                        fill="none"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      >
+                        <path d="M5 12h14"></path>
+                        <path d="M12 5l7 7-7 7"></path>
+                      </svg>
+                    </a>
+                  </div>
                 </div>
               </div>
-            </div>
+            </NuxtLink>
           </div>
-
-          <!-- <div class="p-4 md:w-1/3">
-            <div
-              class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden"
-            >
-              <img
-                class="lg:h-48 md:h-36 w-full object-cover object-center"
-                src="https://dummyimage.com/721x401"
-                alt="blog"
-              />
-              <div class="p-6">
-                <h2
-                  class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1"
-                >
-                  CATEGORY
-                </h2>
-                <h1 class="title-font text-lg font-medium text-gray-900 mb-3">
-                  The 400 Blows
-                </h1>
-                <p class="leading-relaxed mb-3">
-                  Photo booth fam kinfolk cold-pressed sriracha leggings
-                  jianbing microdosing tousled waistcoat.
-                </p>
-                <div class="flex items-center flex-wrap">
-                  <a
-                    class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0"
-                    >Learn More
-                    <svg
-                      class="w-4 h-4 ml-2"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      fill="none"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    >
-                      <path d="M5 12h14"></path>
-                      <path d="M12 5l7 7-7 7"></path>
-                    </svg>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="p-4 md:w-1/3">
-            <div
-              class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden"
-            >
-              <img
-                class="lg:h-48 md:h-36 w-full object-cover object-center"
-                src="https://dummyimage.com/722x402"
-                alt="blog"
-              />
-              <div class="p-6">
-                <h2
-                  class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1"
-                >
-                  CATEGORY
-                </h2>
-                <h1 class="title-font text-lg font-medium text-gray-900 mb-3">
-                  Shooting Stars
-                </h1>
-                <p class="leading-relaxed mb-3">
-                  Photo booth fam kinfolk cold-pressed sriracha leggings
-                  jianbing microdosing tousled waistcoat.
-                </p>
-                <div class="flex items-center flex-wrap">
-                  <a
-                    class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0"
-                    >Learn More
-                    <svg
-                      class="w-4 h-4 ml-2"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      fill="none"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    >
-                      <path d="M5 12h14"></path>
-                      <path d="M12 5l7 7-7 7"></path>
-                    </svg>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div> -->
         </div>
       </div>
     </section>
@@ -301,14 +219,6 @@
         </ul>
       </div>
     </section> -->
-    <!-- <footer class="flex justify-center bg-black">
-      <p class="mt-4 text-white">
-        Articles by
-        <a href="https://it-hires.eu" class="font-bold hover:underline"
-          >IT Hires Europe</a
-        >
-      </p>
-    </footer> -->
   </div>
 </template>
 
