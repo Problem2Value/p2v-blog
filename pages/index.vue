0146312bea9213449a803b2f12e5338585bbb3dc
Geert <geert@problem2value.com>
2021-03-25 11:13:44 +0100

    change color

--- pages/index.vue
+++ pages/index.vue
@@ -15,7 +15,7 @@
         </div>
         <div
           v-else
-          class="sm:w-auto flex-1 text-center bg-purple-100 hover:bg-purple-600 text-white text-lg font-semibold py-3 px-6 border border-transparent rounded-xl focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-gray-900 focus:outline-none transition-colors duration-200"
+          class="sm:w-auto flex-1 text-center bg-transparent hover:bg-purple-600 text-purple-600 text-lg font-semibold py-3 px-6 border border-transparent rounded-xl focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-gray-900 focus:outline-none transition-colors duration-200"
           @click="FilterBlogByType(tag)"
         >
           {{ tag }}
